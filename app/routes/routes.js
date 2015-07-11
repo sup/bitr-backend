dbafc406aadcff276f0799503cab42aa60e51a23
Shikkic <Shikkic@gmail.com>
2015-07-11 13:24:29 -0400

    set up initial route and fixes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,22 @@
-module.exports = function(app, async, conn) {
-	app.get('/', function(req, res){
+module.exports = function(app, async, conn, cps) {
+
+	app.get('/', function(req, res) {
 		res.sendFile('public/index.html');
 	});
+
+    app.get('/db', function(req, res) { 
+       
+         var id = 1;
+        var twitterHandle = "lolbutts";
+        var insert = new cps.InsertRequest('<document><id>'+id+'</id>'+cps.Term(twitterHandle, "twitter-handle")+'</document>');
+        conn.sendRequest(insert, function(err, response) {
+           if (err) {
+                res.send(response);
+                return console.error(err);
+           }
+           console.log('New user registered: ' + response);
+                res.send(response);
+        });
+    });
+
 };
--- index.js
+++ index.js
@@ -6,13 +6,17 @@ var app = express();
 var async = require('async');
 var morgan = require('morgan');
 var cps = require('./node_modules/cps-api');
-var conn = new cps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'bitter', 'USERNAME', 'PASSWORD', 'document', 'document/id', {account: 100781});
+var USERNAME = process.env.CON_USERNAME;
+var PASSWORD = process.env.CON_PASSWORD;
+console.log(USERNAME);
+console.log(PASSWORD);
+var conn = new cps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'bitter', USERNAME, PASSWORD, 'document', 'document/id', {account: 100781});
 
 app.set('port', (process.env.PORT || 3000));
 app.use(express.static('./public'));
 app.use(morgan('dev'));
 
-require('./app/routes/routes.js')(app, async, conn);
+require('./app/routes/routes.js')(app, async, conn, cps);
 
 app.listen(app.get('port'), function() {
 	console.log('listening on port ' + app.get('port'));
