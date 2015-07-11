db58bb93c3a7e624447922fd89a805612be3bf23
Shikkic <Shikkic@gmail.com>
2015-07-11 19:42:08 -0400

    oh man the mergeeeee

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var express = require('express');
 var app = express();
 var async = require('async');
 var morgan = require('morgan');
+var request = require('request');
 var cps = require('./node_modules/cps-api');
+var twitter = require('twitter');
 var USERNAME = process.env.CON_USERNAME;
 var PASSWORD = process.env.CON_PASSWORD;
 console.log(USERNAME);
@@ -16,7 +18,7 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static('./public'));
 app.use(morgan('dev'));
 
-require('./app/routes/routes.js')(app, async, conn, cps);
+require('./app/routes/routes.js')(app, async, request,  conn, cps, twitter);
 
 app.listen(app.get('port'), function() {
 	console.log('listening on port ' + app.get('port'));
