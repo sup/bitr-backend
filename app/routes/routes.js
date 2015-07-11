23c95c06b253e4dffcb566457806ff8bb417882e
Charles Lai <cjl223@cornell.edu>
2015-07-11 19:44:08 -0400

    finish activies endpoints and add json parser

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,5 @@
 module.exports = function(app, async, conn, cps, bodyParser) {
-
+    var jsonParser = bodyParser.json();
     app.get('/', function(req, res) {
         res.sendFile('public/index.html');
     });
@@ -41,56 +41,110 @@ module.exports = function(app, async, conn, cps, bodyParser) {
         conn.sendRequest(search_req, function(err, response) {
             if(!err) {
                 res.send(response.results.document);
-                // TODO Async stuff
             }
             else {
                 console.log(err);
             }
         });
     });
+
     /////////////////////////////////////
     //     GET ACTIVITIES BY RADIUS    //
     /////////////////////////////////////
-    
+    app.get('/map_events', function(req, res) {
+        var logged_user = req.get('Authorization');
+        logged_user = logged_user.split(" ");
+        logged_user = logged_user[1];
+        console.log(logged_user);
+        // Get latitude, longitude, and radius from query variables
+        var lat = req.query.lat;
+        var lng = req.query.lng;
+        var r = req.query.r;
+        var search_req = new cps.SearchRequest("  &gt;&lt;circle", 0, 5);
+        search_req.setParam("shapes", "<circle><center>"+lat+" "+lng+"</center><radius>"+r+" mi</radius><coord1_tag_name>lat</coord1_tag_name><coord2_tag_name>lng</coord2_tag_name></circle>");
+        // Create search object and output response JSON
+        conn.sendRequest(search_req, function(err, response) {
+            if(!err) {
+                if (response.results !== undefined) {
+                    res.send(response.results.document);
+                }
+                else {
+                    res.send("No nearby activities!");
+                }
+            }
+            else {
+                console.log(err);
+            }
+        });
+    });
 
+    /////////////////////////////////////
+    //     UPVOTE ACTIVITIES BY ID     //
+    /////////////////////////////////////
+    app.get('/score', function(req, res) {
+        var logged_user = req.get('Authorization');
+        logged_user = logged_user.split(" ");
+        logged_user = logged_user[1];
+        console.log(logged_user);
+        // Get latitude, longitude, and radius from query variables
+        var activity_id = req.query.activity_id;
+        var vote = req.query.vote === "true";
+        var search_req = new cps.SearchRequest(cps.Term(activity_id, "id"));
+        // Create search object and output response JSON
+        conn.sendRequest(search_req, function(err, response) {
+            if(!err) {
+                if (response.results !== undefined) {
+                    res.send(response.results.document);
+                }
+                else {
+                    res.send("No nearby activities!");
+                }
+            }
+            else {
+                console.log(err);
+            }
+        });
+    });
 
     /////////////////////////////////////
     //     ADD ACTIVITIES BY USER      //
     /////////////////////////////////////
-    app.post('/activities', function(req, res) {
+    app.post('/activities', jsonParser, function(req, res) {
         // Get User
         var logged_user = req.get('Authorization');
         logged_user = logged_user.split(" ");
         logged_user = logged_user[1];
         console.log(logged_user);
         // Get request parameters
-        console.log(req);
-        /*var post_object = JSON.parse(req.body);
+        console.log(req.body);
+        var post_object = req.body;
         var vote = post_object.vote === "true";
         var photo_link = post_object.photo_link;
         var lat = post_object.lat;
         var lng = post_object.lng;
         // Check if this location has been added before by this user
-        var query_string = [user, lat, lng].join(" ");
+        var query_string = [logged_user, lat, lng].join(" ");
         // Create search object and check if the user already was added
         var search_req = new cps.SearchRequest(query_string);
         conn.sendRequest(search_req, function(err, response) {
             // Case: Successful request or error
             if(!err) {
                 // Case: Empty response or existing values
-                if(!response.results.document) {
+                if(response.results === undefined) {
                     var timestamp = new Date() | 0;
-                    var activity_id = timestamp + logged_user;
+                    var activity_id = logged_user + timestamp;
                     var activity_document = '<document>'+
                                                 cps.Term(activity_id, "id") +
-                                                cps.Term(user, "user") +
+                                                cps.Term(logged_user, "user") +
                                                 cps.Term(vote, "vote") +
+                                                cps.Term(0, "score") +
                                                 cps.Term(photo_link, "photo_link") +
                                                 cps.Term(lat, "lat") +
                                                 cps.Term(lng, "lng") +
+                                                cps.Term(timestamp, "timestamp") +
                                             '</document>';
                     var insert = new cps.InsertRequest(activity_document);
-                    conn.sendRequest(search_req, function(err, response) {
+                    conn.sendRequest(insert, function(err, response) {
                         // Case: Activity successfully added or error.
                         if(!err) {
                             res.send({"success": "Activity successfully added."});
@@ -107,6 +161,6 @@ module.exports = function(app, async, conn, cps, bodyParser) {
             else {
                 console.log(err);
             }
-        });*/
+        });
     });
 };
